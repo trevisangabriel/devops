efdb4f8ae6f4d96e4ad5ca9ccfeec31b196dd744
gabriel <gabriel@example.com>
2021-10-22 19:14:16 -0300

    devops

---
+++ 01-terraform_lab_exemplos/05-resource/main.tf
@@ -0,0 +1,87 @@
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_instance" "web" {
+#   ami           = "ami-09e67e426f25ce0d7"
+#   instance_type = "t2.micro"
+#   subnet_id              = "subnet-eddcdzz4"
+#   # availability_zones = ["us-east-1"] # configurando zona para criar maquinas
+#   tags = {
+#     Name = "Minha Maquina Simples EC2"
+#   }
+#   volume_id = aws_ebs_volume.itau_volume_encrypted.id
+# }
+
+
+# resource "aws_ebs_volume" "itau_volume_encrypted" {
+#   size      = 8
+#   encrypted = true
+#   tags      = {
+#     Name = "Volume itaú"
+#   }
+# }
+
+
+
+##### Caso Itaú #####
+# aws configure # maquina pessoal
+# nas do itaú colocar as variáveis de ambiente no bashrc
+# https://docs.aws.amazon.com/sdkref/latest/guide/environment-variables.html
+# configurar via environment variable
+# export AWS_ACCESS_KEY_ID=""
+# export AWS_SECRET_ACCESS_KEY=""
+# export AWS_DEFAULT_REGION=""
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_instance" "web" {
+#   ami = "ami-09e67e426f25ce0d7"
+#   instance_type = "t3.micro"
+#   subnet_id = "subnet-05880ea9006199004"
+  
+#   tags = {
+#     Name = "MinhaPrimeiraMaquina-Carol  "
+#   }
+# }
+
+# resource "aws_ebs_volume" "itau_volume_encrypted" {
+#   size      = 8
+#   encrypted = true
+#   availability_zone = "us-east-1a"
+#   tags      = {
+#     Name = "Volume itaú"
+#   }
+# }
+
+# resource "aws_volume_attachment" "ebs_att" {
+#   device_name = "/dev/sdh"
+#   volume_id   = aws_ebs_volume.itau_volume_encrypted.id
+#   instance_id = aws_instance.web.id
+# }
+
+# Gamaacademythree-sbx - # passago a chave pelo pessoal de segurança itaú
+# export AWS_ACCESS_KEY_ID="3232323232"
+# export AWS_SECRET_ACCESS_KEY="34433444sssdd3+ssa/dd434343"
+
+# //////
+
+# ///////// do fernando zerati //////
+provider "aws" {
+  region = "sa-east-1"
+}
+resource "aws_instance" "web" {
+  subnet_id     = "subnet-05d2f48a5e97f0b1a"
+  ami= "ami-054a31f1b3bf90920"
+  instance_type = "t2.micro"
+  root_block_device {
+    encrypted = true
+    volume_size = 8
+  }
+  tags = {
+    Name = "ec2-zerati-tf"
+  }
+}
+# /////
